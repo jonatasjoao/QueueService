ae6a0c49e03240e1fac8985a780e5ce5faae169a
jonatasjoao <jonatas.silva@nycbank.com.br>
2023-11-07 18:17:40 -0300

    add TransactionQueueController.cs

---
+++ QueueService.API/Controllers/TransactionQueueController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QueueService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransactionQueueController : Controller
+{
+   
+}
