c63121bfb9d818f2dcfe2da2beedee8d86a0c73f
jonatasjoao <jonatas.silva@nycbank.com.br>
2023-11-07 18:17:28 -0300

    implement RepositoryBase.cs

--- QueueService.Infra/Repositories/RepositoryBase.cs
+++ QueueService.Infra/Repositories/RepositoryBase.cs
@@ -1,31 +1,41 @@
-﻿using QueueService.Infra.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using QueueService.Infra.Interfaces;
 
 namespace QueueService.Infra.Repositories;
 
 public abstract class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
 {
+    protected readonly DbContext Db;
+    protected readonly DbSet<TEntity> DbSet;
+
+    public RepositoryBase(DbContext context)
+    {
+        Db = context;
+        DbSet = Db.Set<TEntity>();
+    }
     public void Dispose()
     {
-        throw new NotImplementedException();
+        Db.Dispose();
+        GC.SuppressFinalize(this);
     }
 
     public TEntity GetById(long ID)
     {
-        throw new NotImplementedException();
+        return DbSet.Find(ID);
     }
 
     public void Insert(TEntity entity)
     {
-        throw new NotImplementedException();
+        DbSet.Add(entity);
     }
 
     public void Update(TEntity entity)
     {
-        throw new NotImplementedException();
+        DbSet.Update(entity);
     }
 
     public IQueryable<TEntity> GetAll()
     {
-        throw new NotImplementedException();
+        return DbSet;
     }
 }
